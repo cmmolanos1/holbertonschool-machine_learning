643079758c475ba08bd85f639d203c7d5e5a1106
Carlos Molano <926@holbertonschool.com>
2020-04-13 11:30:38 -0500

    [Upload]: Task 9

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -19,5 +19,4 @@ def mat_mul(mat1, mat2):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 result[i][j] += mat1[i][k] * mat2[k][j]
-
     return result
