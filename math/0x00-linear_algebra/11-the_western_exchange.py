e5973991acbd96a96c54a770ffaf2511ddb3f48b
Carlos Molano <926@holbertonschool.com>
2020-04-13 11:46:18 -0500

    [Upload]: Task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Matrix Transpose"""
+import numpy
+
+
+def np_transpose(matrix):
+    """
+    Transpose the matrix, or change rows by cols
+    :param matrix: (ndarray)
+    :return: (ndarray)
+    """
+    return numpy.transpose(matrix)
