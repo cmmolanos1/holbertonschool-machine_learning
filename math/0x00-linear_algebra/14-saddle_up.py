1ba7604c51aaeee3f170a08caaeb5c796440ff97
Carlos Molano <926@holbertonschool.com>
2020-04-13 12:09:32 -0500

    [Upload]: Task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication
+    :param mat1: (ndarray)
+    :param mat2: (ndarray)
+    :return: (ndarray)
+    """
+    return np.matmul(mat1, mat2)
