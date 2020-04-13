085daf6e490d23c9bb5d3efda520e9378edd73ad
Carlos Molano <926@holbertonschool.com>
2020-04-13 11:40:44 -0500

    [Upload]: Task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Matrix shape"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of matrix
+    :param matrix: (ndarray)
+    :return: (tuple) the shape of matrix
+    """
+    return matrix.shape
