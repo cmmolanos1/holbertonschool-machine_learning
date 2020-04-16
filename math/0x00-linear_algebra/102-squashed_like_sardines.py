0e2f4662626ab821884700a5bbb39033a45d8fb0
Carlos Molano <926@holbertonschool.com>
2020-04-15 19:34:45 -0500

    [Upload]: Task 17

---
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""n-dimensional Matrix addition"""
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """Concatenates n-dimensional matrices.
+
+    Args:
+        mat1 (list): first matrix
+        mat2 (list): second matrix
+        axis (list):  concatenated matrix
+
+    Returns:
+
+    """
+    pass
