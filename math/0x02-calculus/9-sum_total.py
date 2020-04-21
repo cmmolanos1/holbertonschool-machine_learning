130aa47c3bb185ef69976c8109256a8e9b367c8e
Carlos Molano <926@holbertonschool.com>
2020-04-21 11:00:04 -0500

    [Upload]: Add Task 9.

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Summation"""
+
+
+def summation_i_squared(n):
+    """Summation of i**2, from i = 1 to i = n
+
+    Args:
+        n (int): stop number
+
+    Returns:
+        int: summation from 1 to n
+    """
+    if n < 1:
+        return None
+    elif n == 1:
+        return 1
+    else:
+        return n ** 2 + summation_i_squared(n - 1)
