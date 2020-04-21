7c42124412f2a34144a0ed0ff9964925892db444
Carlos Molano <926@holbertonschool.com>
2020-04-21 11:59:25 -0500

    [Upload]: Add Task 10.

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Derivative"""
+
+
+def poly_derivative(poly):
+    """Performs a derivative of a polynomial.
+    Ej:
+    d(5 + 3x + x^3)/dx ----> 3 + 3x^2
+    [5, 3, 0, 1]       ----> [3, 0, 3]
+
+    Args:
+        poly (list): the polinomial ordered by coefficients.
+
+    Returns:
+        list: the derivated polynomial.
+    """
+    # Check if poly is a valid list.
+    if poly == [] or type(poly) is not list:
+        return None
+    for n in poly:
+        if type(n) is not int and type(n) is not float:
+            return None
+
+    # Create a list with X-exponents
+    exponents = [n for n in range(len(poly))]
+
+    if len(poly) == 1:
+        return [0]
+    else:
+        # Multiply exponents and coefficients, and delete the constant.
+        result = [exponents[i] * poly[i] for i in range(1, len(poly))]
+
+        return result if any(result) else [0]
