c0bf7ed4b913c3e240ebd9ee1a8b2919c7d070d8
Carlos Molano <926@holbertonschool.com>
2020-04-24 07:15:07 -0500

    [Fix]: Alternative way.

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -11,9 +11,8 @@ def summation_i_squared(n):
     Returns:
         int: summation from 1 to n
     """
-    if n < 0 or type(n) is not int or n is None:
+    if n < 1 or type(n) is not int or n is None:
         return None
-    elif n == 0:
-        return 0
+
     else:
-        return n ** 2 + summation_i_squared(n - 1)
+        return int(n * (n + 1) * (2 * n + 1) / 6)
