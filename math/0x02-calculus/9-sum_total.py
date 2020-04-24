9db6450ac5635dc0ba0625a50b2ef83c82718637
Carlos Molano <926@holbertonschool.com>
2020-04-24 07:08:27 -0500

    [Fix]:

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -11,8 +11,9 @@ def summation_i_squared(n):
     Returns:
         int: summation from 1 to n
     """
-    if n <= 0 or type(n) is not int:
+    if n < 1 or type(n) is not int or n is None:
         return None
+    elif n == 1:
+        return 1
     else:
-        s = sum([n ** 2 for n in range(1, n + 1)])
-        return s
+        return n ** 2 + summation_i_squared(n - 1)
