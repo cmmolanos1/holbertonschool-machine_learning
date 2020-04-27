c5b4a279844e4f2f379570c1c837c63c6c566a26
Carlos Molano <926@holbertonschool.com>
2020-04-27 12:19:02 -0500

    [Fix]: Upload task 1.

--- math/0x03-probability/README.md
+++ math/0x03-probability/README.md
@@ -21,3 +21,22 @@ Lambtha: 4.84
 print('Lambtha:', p2.lambtha)
 Lambtha: 5.0
 ```
+
+### 1. Poisson PMF 
+
+File: `poisson.py`
+
+Create the instance method to calculate the Probability Mass Function(f(x;λ)) `def pmf(self, k):`, where `k` is the number of “successes”.
+
+```python
+>>> import numpy as np
+>>> from poisson import Poisson
+>>> np.random.seed(0)
+>>> data = np.random.poisson(5., 100).tolist()
+>>> p1 = Poisson(data)
+>>> print('P(9):', p1.pmf(9))
+P(9): 0.03175849616802446
+>>> p2 = Poisson(lambtha=5)
+>>> print('P(9):', p2.pmf(9))
+P(9): 0.036265577412911795
+```
--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Class Poisson"""
 
+e = 2.7182818285
+
 
 class Poisson():
     def __init__(self, data=None, lambtha=1.):
@@ -16,4 +18,18 @@ class Poisson():
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             else:
-                self.lambtha = sum(data) / len(data)
+                self.data = data
+                self.lambtha = sum(self.data) / len(self.data)
+
+    def pmf(self, k):
+        x = int(k)
+        if (isinstance(self.data, float) and x > 1) or \
+                (isinstance(self.data, list) and x > len(self.data)):
+            return 0
+        else:
+            x_fact = 1
+            for i in range(1, x + 1):
+                x_fact = x_fact * i
+
+            f = e ** -self.lambtha * self.lambtha ** x / x_fact
+            return f
