2ce23b51764054d72c6c1963179ac9fb87eeffe8
Carlos Molano <926@holbertonschool.com>
2020-05-11 09:16:05 -0500

    [Upload]: Add documentation.

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -4,6 +4,15 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
+    """that converts a numeric label vector into a one-hot matrix
+
+    Args:
+        Y (numpy.ndarray): numeric class labels.
+        classes (int): maximum number of classes.
+
+    Returns:
+        numpy.ndarray: one hot matrix.
+    """
     if Y[Y < 0] or Y[Y >= classes]:
         return 0
 
--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -4,6 +4,14 @@ import numpy as np
 
 
 def one_hot_decode(one_hot):
+    """Converts a one-hot matrix into a vector of labels.
+
+    Args:
+        one_hot (numpy.ndarray):  one how matrix with shape (classes, m).
+
+    Returns:
+        numpy.ndarray: labels vector.
+    """
     if isinstance(one_hot, np.ndarray) is False:
         return 0
 
