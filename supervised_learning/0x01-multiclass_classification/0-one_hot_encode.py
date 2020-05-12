9a321cacb8ed8e1078e7aecf5148e2a23ce57bcc
Carlos Molano <926@holbertonschool.com>
2020-05-12 00:09:01 -0500

    [Fix] Task 0: Add Documentation.

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Ont hot"""
 import numpy as np
 
 
--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -16,3 +16,19 @@ def one_hot_decode(one_hot):
         return 0
 
     return np.argmax(one_hot, axis=0)
+
+# def one_hot_decode(one_hot):
+#     """ converts a one-hot matrix into a vector of labels """
+#     if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
+#         return None
+#     if not np.where((one_hot == 0) | (one_hot == 1), True, False).all():
+#         return None
+#     if np.sum(one_hot) != len(one_hot[0]):
+#         return None
+#     classes = len(one_hot)
+#     m = len(one_hot[0])
+#     samples = np.zeros(m)
+#     tmp = np.arange(m)
+#     axis = np.argmax(one_hot, axis=0)
+#     samples[tmp] = axis
+#     return samples.astype("int64")
