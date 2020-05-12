6e5499d75eea3064c45ecdcc1176e01df0ffe15b
Carlos Molano <926@holbertonschool.com>
2020-05-12 00:32:10 -0500

    [Fix] Task 1: Add condition.

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+""" One hot"""
 import numpy as np
 
 
@@ -12,23 +12,11 @@ def one_hot_decode(one_hot):
     Returns:
         numpy.ndarray: labels vector.
     """
-    if isinstance(one_hot, np.ndarray) is False:
-        return 0
+    if not isinstance(one_hot, np.ndarray):
+        return None
+    if not isinstance(one_hot[0], np.integer):
+        return None
+    if not np.all((one_hot == 0) | (one_hot == 1)):
+        return None
 
     return np.argmax(one_hot, axis=0)
-
-# def one_hot_decode(one_hot):
-#     """ converts a one-hot matrix into a vector of labels """
-#     if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
-#         return None
-#     if not np.where((one_hot == 0) | (one_hot == 1), True, False).all():
-#         return None
-#     if np.sum(one_hot) != len(one_hot[0]):
-#         return None
-#     classes = len(one_hot)
-#     m = len(one_hot[0])
-#     samples = np.zeros(m)
-#     tmp = np.arange(m)
-#     axis = np.argmax(one_hot, axis=0)
-#     samples[tmp] = axis
-#     return samples.astype("int64")
