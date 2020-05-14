2414e9b755b17bc5669b63bd0e7caf9c191b26aa
Carlos Molano <926@holbertonschool.com>
2020-05-14 17:13:45 -0500

    [Upload] Task 6.

---
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Train"""
+import tensorflow as tf
+
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
+    """Builds, trains, and saves a neural network classifier.
+
+    Args:
+        X_train (np.array): training input data.
+        Y_train (np.array): training labels.
+        X_valid (np.array): validation input data.
+        Y_valid (np.array): validation labels.
+        layer_sizes (list): list containing the number of nodes in each
+                            layer of the network.
+        activations (list): list containing the activation functions for each
+                            layer of the network.
+        alpha (float): learning rate.
+        iterations (int): number of iterations to train over.
+        save_path (str): where to save the model
+
+    Returns:
+        str: the path where the model was saved
+    """
+    m, nx = X_train.shape
+    ny = Y_train.shape[1]
+
+    x, y = create_placeholders(nx, ny)
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    cost = calculate_loss(y, y_pred)
+    optimizer = create_train_op(cost, alpha)
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(iterations + 1):
+
+            cost_t, accuracy_t = sess.run([cost, accuracy],
+                                          feed_dict={x: X_train, y: Y_train})
+            cost_v, accuracy_v = sess.run([cost, accuracy],
+                                          feed_dict={x: X_valid, y: Y_valid})
+
+            if i % 100 == 0:
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(cost_t))
+                print("\tTraining Accuracy: {}".format(accuracy_t))
+                print("\tValidation Cost: {}".format(cost_v))
+                print("\tValidation Accuracy: {}".format(accuracy_v))
+
+            if i < iterations:
+                sess.run(optimizer, feed_dict={x: X_train, y: Y_train})
+
+        return saver.save(sess, save_path)
