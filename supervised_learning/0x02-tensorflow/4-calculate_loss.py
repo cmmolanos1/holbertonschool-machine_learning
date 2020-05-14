a1ede3174efa5c5b4c2821e99d23ba2f920bde3b
Carlos Molano <926@holbertonschool.com>
2020-05-14 13:32:59 -0500

    [Upload] Task 4.

---
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Loss"""
+import tensorflow as tf
+
+
+def calculate_loss(y, y_pred):
+    """Calculates the softmax cross-entropy loss of a prediction.
+
+    Args:
+        y (tensor): placeholder for the labels of the input data.
+        y_pred (tensor): the network’s predictions.
+
+    Returns:
+        tensor: the loss of the prediction
+    """
+    return tf.losses.softmax_cross_entropy(y, y_pred)
