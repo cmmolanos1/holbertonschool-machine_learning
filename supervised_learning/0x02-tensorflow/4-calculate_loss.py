a1842e7e0baee37bca4c4c829085ffd75898de46
Carlos Molano <926@holbertonschool.com>
2020-05-16 09:37:21 -0500

    [Fix] Assign before return.

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-"""Loss"""
+"""cost"""
+
 import tensorflow as tf
 
 
 def calculate_loss(y, y_pred):
-    """Calculates the softmax cross-entropy loss of a prediction.
-
-    Args:
-        y (tensor): placeholder for the labels of the input data.
-        y_pred (tensor): the network’s predictions.
-
-    Returns:
-        tensor: the loss of the prediction
     """
-    loss = tf.losses.softmax_cross_entropy(y, y_pred)
-    return loss
+    y is a placeholder for the labels of the input data
+    y_pred is a tensor containing the networks predictions
+    Returns: a tensor containing the loss of the prediction
+    """
+    return tf.losses.softmax_cross_entropy(y, y_pred)
