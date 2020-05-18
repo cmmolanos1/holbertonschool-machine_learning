e0987747b705dda695f31a3bd94e5a9b09957f7f
Carlos <926@holbertonschool.com>
2020-05-18 10:15:58 -0500

    [Upload] task2.

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Shuffle
+"""
+
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """Shuffles the data points in two matrices the same way.
+
+    Args:
+        X (np.ndarray): (m, nx) matrix to shuffle.
+        Y (np.ndarray): (m, nx) matrix to shuffle.
+
+    Returns:
+        np.ndarray: shiffled version of X and Y.
+
+    """
+    m = X.shape[0]
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[permutation, :]
+    shuffled_Y = Y[permutation, :]
+
+    return shuffled_X, shuffled_Y
