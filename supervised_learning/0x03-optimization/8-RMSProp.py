f60dae8e8d9be75e3f156db88c2201fdab785f7b
Carlos <926@holbertonschool.com>
2020-05-21 12:20:39 -0500

    [Fix].

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -19,5 +19,8 @@ def create_RMSProp_op(loss, alpha, beta2, epsilon):
     Returns:
         tensor:  the RMSProp optimization operation.
     """
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
-    return optimizer.minimize(loss)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                          decay=beta2,
+                                          epsilon=epsilon)
+    train = optimizer.minimize(loss)
+    return train
