3640421d15cca55d1d29c667b4ead677d19553a1
Carlos <926@holbertonschool.com>
2020-05-22 20:49:13 -0500

    [Upload] Readme

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -173,6 +173,7 @@ def calculate_loss(y, y_pred):
     """
     return tf.losses.softmax_cross_entropy(y, y_pred)
 
+
 def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
           beta2=0.999, epsilon=1e-8, decay_rate=1, batch_size=32, epochs=5,
           save_path='/tmp/model.ckpt'):
