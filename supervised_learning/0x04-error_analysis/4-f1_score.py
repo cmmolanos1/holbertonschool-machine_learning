b6f1f3a237d9201020a4173da3eb211bbdb018f9
Carlos <926@holbertonschool.com>
2020-05-26 08:19:44 -0500

    [Fix]

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-F1
+F1 Score
 """
 
 sensitivity = __import__('1-sensitivity').sensitivity
@@ -11,15 +11,16 @@ def f1_score(confusion):
     """Calculates the F1-score for each class in a confusion matrix.
 
         Args:
-            confusion (np.ndarray): confusion matrix of shape(classes,classes)
+            confusion: confusion matrix of shape(classes,classes)
                                     where row indices represent the correct
                                     labels and column indices represent the
                                     predicted labels.
 
         Returns:
-            np.ndarray: array of shape (classes,) containing the F1-score of
+            np.array: array of shape (classes,) containing the F1-score of
                         each class.
     """
     PPV = precision(confusion)
     TPR = sensitivity(confusion)
-    return 2 * PPV * TPR / (PPV + TPR)
+    F1 = 2 * PPV * TPR / (PPV + TPR)
+    return F1
