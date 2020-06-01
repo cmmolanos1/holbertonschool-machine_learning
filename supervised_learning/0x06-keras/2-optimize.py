b59a68ae4e5dd4832b06cb6bfcdcac64d73c6e26
Carlos <926@holbertonschool.com>
2020-06-01 10:53:31 -0500

    [Upload] Task 2.

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Optimize
+"""
+import tensorflow as tf
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """Sets up Adam optimization for a keras model with categorical
+    crossentropy loss and accuracy metrics
+
+    Args:
+        network: the model to optimize.
+        alpha: learning rate.
+        beta1: first Adam optimization parameter.
+        beta2: Second Adam optimization parameter.
+    """
+    network.compile(optimizer=tf.keras.optimizers.Adam(lr=alpha,
+                                                       beta_1=beta1,
+                                                       beta_2=beta2),
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+    return None
