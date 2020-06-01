ff0711db7da420d13663a5443ee62026910f27a9
Carlos <926@holbertonschool.com>
2020-06-01 14:15:33 -0500

    [Upload] Task 13.

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Prediction
+"""
+
+
+def predict(network, data, verbose=False):
+    """Makes a prediction using a neural network.
+
+    Args:
+        network (Keras model): network model to make the prediction with.
+        data (np.ndarray): input data to make the prediction with.
+        verbose (bool): determines if output should be printed during the
+                        prediction process.
+
+    Returns:
+        The prediction for the data.
+    """
+    return network.predict(data, verbose=verbose)
