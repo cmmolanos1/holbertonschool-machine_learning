f78b6e0428ded3c9bd0e1d2a139655fff766dd4b
Carlos <926@holbertonschool.com>
2020-06-01 14:08:09 -0500

    [Upload] Task 12.

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Test
+"""
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+
+    Args:
+        network (Keras model): the network model to test.
+        data (np.ndarray): input data to test the model with.
+        labels (np.ndarray): correct one-hot labels of data.
+        verbose (bool): determines if output should be printed during the
+                        testing process.
+
+    Returns:
+        float: the loss and accuracy of the model with the testing data,
+               respectively
+    """
+    return network.evaluate(data, labels, verbose=verbose)
