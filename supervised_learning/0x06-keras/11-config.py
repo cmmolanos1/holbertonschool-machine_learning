5ae57cb0a075098b0f459810b2ec5e8c93713120
Carlos <926@holbertonschool.com>
2020-06-01 13:57:29 -0500

    [Upload] Task 11.

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Config
+"""
+
+from tensorflow import keras
+
+
+def save_config(network, filename):
+    """Saves a model’s configuration in JSON format.
+
+    Args:
+        network: model whose configuration should be saved.
+        filename: path of the file that the configuration should be saved to.
+
+    Returns:
+        None:
+    """
+    model_json = network.to_json()
+    with open(filename, "w") as json_file:
+        json_file.write(model_json)
+
+
+def load_config(filename):
+    """Loads a model with a specific configuration
+
+    Args:
+        filename: path of the file containing the model’s configuration in
+                  JSON format.
+
+    Returns:
+        The loaded model.
+    """
+    with open(filename, "r") as json_file:
+        loaded_model_json = json_file.read()
+
+    loaded_model = keras.models.model_from_json(loaded_model_json)
+
+    return loaded_model
