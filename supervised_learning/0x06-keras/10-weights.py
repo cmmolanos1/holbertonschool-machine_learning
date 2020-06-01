8cae1cbe570e0bb3ec094c50a127835951ca9a8e
Carlos <926@holbertonschool.com>
2020-06-01 13:57:55 -0500

    [Upload] Task 10.

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Weights
+"""
+
+
+def save_weights(network, filename, save_format='h5'):
+    """Saves a model’s weights.
+
+    Args:
+        network: model whose weights should be saved.
+        filename: the path of the file that the weights should be saved to.
+        save_format: format in which the weights should be saved.
+
+    Returns:
+        None.
+    """
+    network.save_weights(filename, save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    """Loads a model’s weights
+
+    Args:
+        network: model to which the weights should be loaded.
+        filename: path of the file that the weights should be loaded from.
+
+    Returns:
+        None
+    """
+    network.load_weights(filename)
