efe6bd4252e0a7f6723b3b734ba4ad0be119e9ea
Carlos <926@holbertonschool.com>
2020-06-03 12:17:52 -0500

    [Upload] Task1

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """Performs a same convolution on grayscale images.
+
+    Args:
+        images (np.ndarray): matrix of shape (m, h, w) containing multiple
+                             grayscale images.
+        kernel (np.ndarray): matrix of shape (kh, kw) containing the kernel
+                             for the convolution.
+
+    Returns:
+        np.ndarray: The convolved images.
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    ch = h - kh + 1
+    cw = w - kw + 1
+
+    if ch != h or cw != w:
+        ph = int((kh - 1) / 2)
+        pw = int((kw - 1) / 2)
+        padded_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+
+    convolved = np.zeros((m, h, w))
+
+    for j in range(ch):
+        for k in range(cw):
+            images_slide = padded_img[:, k:k + kh, j:j + kw]
+            elem_mul = np.multiply(images_slide, kernel)
+            convolved[:, k, j] = elem_mul.sum(axis=1).sum(axis=1)
+
+    return convolved
