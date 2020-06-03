ad5f3b845fe363d5b582c40773198e085fc6bbb3
Carlos <926@holbertonschool.com>
2020-06-03 15:31:18 -0500

    [Upload] upload task 5.

--- math/0x04-convolutions_and_pooling/5-convolve.py
+++ math/0x04-convolutions_and_pooling/5-convolve.py
@@ -3,14 +3,15 @@
 import numpy as np
 
 
-def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
-    """Performs a convolution on images with channels.
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    """Performs a convolution on images with multiple kernels.
 
     Args:
         images (np.ndarray): matrix of shape (m, h, w, c) containing multiple
                              grayscale images.
-        kernel (np.ndarray): matrix of shape (kh, kw, c) containing the kernel
-                             for the convolution.
+        kernels (np.ndarray): matrix of shape (kh, kw, c, nc) containing the
+                              kernel for the convolution. nc stands for number
+                              of kernels.
         padding: if same: performs same padding,
                  if valid: valid padding.
                  if tuple (ph, pw) = padding for the height,
@@ -20,7 +21,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         np.ndarray: The convolved images.
     """
     m, h, w, c = images.shape
-    kh, kw, _ = kernel.shape
+    kh, kw, _, nc = kernels.shape
     sh, sw = stride
 
     if padding == 'same':
@@ -40,16 +41,18 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     ch = int((h + 2 * ph - kh) / sh) + 1
     cw = int((w + 2 * pw - kw) / sw) + 1
 
-    convolved = np.zeros((m, ch, cw))
+    convolved = np.zeros((m, ch, cw, c))
 
-    for j in range(ch):
-        for k in range(cw):
-            images_slide = padded_img[:,
-                                      j * sh:j * sh + kh,
-                                      k * sw:k * sw + kw]
+    for n in range(nc):
+        for j in range(ch):
+            for k in range(cw):
 
-            elem_mul = np.multiply(images_slide, kernel)
-            convolved[:, j, k] = elem_mul.sum(axis=1).sum(axis=1).\
-                sum(axis=1)
+                images_slide = padded_img[:,
+                                          j * sh:j * sh + kh,
+                                          k * sw:k * sw + kw]
+                kernel = kernels[:, :, :, n]
+                elem_mul = np.multiply(images_slide, kernel)
+                convolved[:, j, k, n] = elem_mul.sum(axis=1).sum(axis=1).\
+                    sum(axis=1)
 
     return convolved
