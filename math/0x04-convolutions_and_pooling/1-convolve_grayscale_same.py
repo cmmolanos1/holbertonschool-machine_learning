f4ce426fd45fcbbd8c375617609b36e5465530ea
Carlos <926@holbertonschool.com>
2020-06-04 12:40:19 -0500

    [Fix] delete if.

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -21,18 +21,14 @@ def convolve_grayscale_same(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    ch = h - kh + 1
-    cw = w - kw + 1
-
-    if ch != h or cw != w:
-        ph = int(kh / 2)
-        pw = int(kw / 2)
-        padded_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    ph = int(kh / 2)
+    pw = int(kw / 2)
+    padded_img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
 
     convolved = np.zeros((m, h, w))
 
-    for j in range(ch):
-        for k in range(cw):
+    for j in range(h):
+        for k in range(w):
             images_slide = padded_img[:, j:j + kh, k:k + kw]
             elem_mul = np.multiply(images_slide, kernel)
             convolved[:, j, k] = elem_mul.sum(axis=1).sum(axis=1)
