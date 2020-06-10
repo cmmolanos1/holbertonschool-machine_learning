1b8087c4c9fa982fe5038c3bc8092900a7db8a30
Carlos <926@holbertonschool.com>
2020-06-10 15:35:16 -0500

    [Fix] Refactoring code.

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -39,10 +39,8 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     pw = 0
 
     if padding == 'same':
-        ph = np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2)
-        ph = int(ph)
-        pw = np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2)
-        pw = int(pw)
+        ph = int(np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2))
+        pw = int(np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2))
 
     A_prev_pad = np.pad(A_prev,
                         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
@@ -65,7 +63,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
                     dW[:, :, :, c] += A_prev_pad[i,
                                                  vert_start:vert_end,
-                                                 horiz_start:horiz_end:,
+                                                 horiz_start:horiz_end,
                                                  :] * dZ[i, h, w, c]
 
     if padding == 'same':
