64eb9ed555132d0d04f62afadbbdb32599d123ee
Carlos <926@holbertonschool.com>
2020-06-15 19:54:59 -0500

    [Upload] Task3.

---
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """Builds a projection block as described in Deep Residual Learning for
+    Image Recognition (2015).
+
+    Args:
+        A_prev (Keras layer): the output from the previous layer.
+        filters (list or tuple): [F11, F3, F12]
+                                 F11 is the number of filters in the first 1x1
+                                 convolution.
+                                 F3 is the number of filters in the 3x3
+                                 convolution.
+                                 F12 is the number of filters in the second
+                                 1x1 convolution as well as the 1x1
+                                 convolution in the shortcut connection.
+        s (int): stride of the first convolution in both the main path and the
+                 shortcut connection.
+    Returns:
+        The activated output of the identity block.
+    """
+    conv1 = K.layers.Conv2D(filters=filters[0],
+                            kernel_size=1,
+                            padding='same',
+                            strides=(s, s),
+                            kernel_initializer='he_normal')(A_prev)
+
+    batch1 = K.layers.BatchNormalization()(conv1)
+
+    relu1 = K.layers.Activation('relu')(batch1)
+
+    conv2 = K.layers.Conv2D(filters=filters[1],
+                            kernel_size=3,
+                            padding='same',
+                            # strides=(s, s),
+                            kernel_initializer='he_normal')(relu1)
+
+    batch2 = K.layers.BatchNormalization()(conv2)
+
+    relu2 = K.layers.Activation('relu')(batch2)
+
+    conv3 = K.layers.Conv2D(filters=filters[2],
+                            kernel_size=1,
+                            padding='same',
+                            # strides=(s, s),
+                            kernel_initializer='he_normal')(relu2)
+
+    batch3 = K.layers.BatchNormalization()(conv3)
+
+    # Shortcut connection.
+    conv_sc = K.layers.Conv2D(filters=filters[2],
+                              kernel_size=1,
+                              padding='same',
+                              strides=(s, s),
+                              kernel_initializer='he_normal')(A_prev)
+
+    batch_sc = K.layers.BatchNormalization()(conv_sc)
+
+    adding = K.layers.Add()([batch3, batch_sc])
+
+    output = K.layers.Activation('relu')(adding)
+
+    return output
