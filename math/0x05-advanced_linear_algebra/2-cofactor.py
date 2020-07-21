8cce17bb4d2faac62645b45909cd64e594ada502
Carlos <926@holbertonschool.com>
2020-07-21 14:46:28 -0500

    [Upload] task2-3-4

---
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""
+Determinant
+"""
+
+
+def omite(m, index):
+    """Omite the 1st row and index column of a square matrix.
+
+    Args:
+        m (list): matrix.
+        index (int): column to omite.
+
+    Returns:
+        the matrix with the omited row, column.
+    """
+    return [[m[i][j] for j in range(len(m[i])) if j != index]
+            for i in range(1, len(m))]
+
+
+def determinant(matrix):
+    """ Calculates the determinant of a square matrix.
+
+    Args:
+        matrix (list): matrix to calculate.
+
+    Returns:
+        the determinant.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix[0] and len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if matrix == [[]]:
+        return 1
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for j in range(len(matrix[0])):
+        omited_matrix = omite(matrix, j)
+        det += matrix[0][j] * ((-1) ** j) * determinant(omited_matrix)
+
+    return det
+
+
+def minor_m(m, row, col):
+    """Omite the the given row and column of a square matrix.
+
+    Args:
+        m (list): matrix.
+        row (int): row to omite.
+        col (int): column to omite.
+
+    Returns:
+        the matrix with the omited row, column.
+    """
+    return [[m[i][j] for j in range(len(m[i])) if j != col]
+            for i in range(len(m)) if i != row]
+
+
+def cofactor(matrix):
+    """ Calculates the cofactor of a square matrix.
+
+    Args:
+        matrix (list): matrix to calculate.
+
+    Returns:
+        the determinant.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if all([type(i) is list for i in matrix]) is False:
+        raise TypeError("matrix must be a list of lists")
+
+    if (matrix[0] and len(matrix) != len(matrix[0])) or \
+            matrix == [] or matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return [[1]]
+
+    return [[((-1) ** (i + j)) * determinant(minor_m(matrix, i, j))
+             for j in range(len(matrix[i]))] for i in range(len(matrix))]
