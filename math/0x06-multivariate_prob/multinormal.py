17a222e6965e621ac427e5e4e1997bb53556a27d
Carlos <926@holbertonschool.com>
2020-07-27 13:07:58 -0500

    [Upload] Task 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -15,17 +15,16 @@ class MultiNormal():
         """
         Class constructor
         """
-        if (not type(data) == np.ndarray) or (len(data.shape) != 2):
+        if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        n = data.shape[1]
+        d, n = data.shape
         if n < 2:
             raise ValueError("data must contain multiple data points")
 
-        d = data.shape[0]
         self.mean = (np.mean(data, axis=1)).reshape(d, 1)
 
         X = data - self.mean
-        self.cov = ((np.dot(X, X.T)) / (n - 1))
+        self.cov = ((np.matmul(X, X.T)) / (n - 1))
 
     def pdf(self, x):
         """Calculates the Probability distribution function at a data point.
@@ -37,21 +36,22 @@ class MultiNormal():
         Returns:
 
         """
-        if type(x) is not np.ndarray:
-            raise TypeError("x must by a numpy.ndarray")
-
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
         d = self.cov.shape[0]
         if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
             raise ValueError("x must have the shape ({}, 1)".format(d))
 
-        x_minusu = x - self.mean
-        cov_det = np.linalg.det(self.cov)
-        cov_inv = np.linalg.inv(self.cov)
+        # pdf formula -- multivar
 
-        pdf1 = 1 / np.sqrt(((2 * np.pi) ** d) * cov_det)
-        pdf2 = np.exp(-0.5 * np.matmul(np.matmul(x_minusu.T, cov_inv),
-                                       x_minusu))
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        f1 = 1 / np.sqrt(((2 * np.pi) ** d) * det)
+        f21 = -(x - self.mean).T
+        f22 = np.matmul(f21, inv)
+        f23 = (x - self.mean) / 2
+        f24 = np.matmul(f22, f23)
+        f2 = np.exp(f24)
+        pdf = f1 * f2
 
-        pdf = pdf1 * pdf2
-
-        return pdf.flatten()[0]
+        return pdf.reshape(-1)[0]
