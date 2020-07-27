24a26926d9eba6884d4c23c9333b13d6482070ff
Carlos <926@holbertonschool.com>
2020-07-27 12:24:15 -0500

    [Upload] Task 1

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -17,13 +17,12 @@ def correlation(C):
     if type(C) is not np.ndarray:
         raise TypeError("C must be a numpy.ndarray")
 
-    if len(C.shape) != 2:
-        raise ValueError(" must be a 2D square matrix")
-    if C.shape[0] != C.shape[1]:
-        raise ValueError(" must be a 2D square matrix")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
 
     variance = np.sqrt(np.diag(C))
     outer_v = np.outer(variance, variance)
     correlation = C / outer_v
+    correlation[C == 0] = 0
 
     return correlation
