e2dad147b96ff75319573bc35fe83cf6c47de1db
Carlos <926@holbertonschool.com>
2020-07-29 21:37:34 -0500

    [Upload]

---
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -0,0 +1,17 @@
+"""
+PSA
+"""
+
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """Performs PCA on a dataset
+    """
+    u, s, vh = np.linalg.svd(X)
+    total_variance = np.cumsum(s) / np.sum(s)
+    r = (np.argwhere(total_variance >= var))[0, 0]
+
+    weight = vh[:r + 1].T
+
+    return weight
---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"PCA"
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Performs PCA on a dataset
+    """
+    X_m = X - np.mean(X, axis=0)
+
+    u, s, vh = np.linalg.svd(X_m)
+    W = vh[:ndim].T
+
+    T = np.matmul(X_m, (W))
+
+    return T
