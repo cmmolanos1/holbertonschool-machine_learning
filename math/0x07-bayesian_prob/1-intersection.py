5e7e4f2e9dd26d3694bf46b3cc543cd860d8b32c
Carlos <926@holbertonschool.com>
2020-07-31 12:27:23 -0500

    [Fix] condition

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -21,23 +21,24 @@ def intersection(x, n, P, Pr):
         1D numpy.ndarray containing the intersection of obtaining x and n with
         each probability in P, respectively.
     """
-    if not isinstance(n, int) or (n <= 0):
+    if not isinstance(n, (int, float)) or (n <= 0):
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or (x < 0):
+    if not isinstance(x, (int, float)) or (x < 0):
         raise ValueError(
             "x must be an integer that is greater than or equal to 0")
-    if x > n:
+    if (x > n):
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if not np.all((P >= 0) & (P <= 1)):
+    if not isinstance(Pr, np.ndarray) or (P.shape != Pr.shape):
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P")
+    if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
-    if not np.all((Pr >= 0) & (Pr <= 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
-    suma = np.sum(Pr)
-    if not np.isclose(suma, 1):
+    if np.any(Pr > 1) or np.any(Pr < 0):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    suma = (np.sum(Pr))
+    if not (np.isclose(suma, 1)):
         raise ValueError("Pr must sum to 1")
 
     combination = np.math.factorial(n) / (np.math.factorial(n - x) *
