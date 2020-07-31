662b3d0653dfe004cbb10f0c88d873d75c0f64b3
Carlos <926@holbertonschool.com>
2020-07-31 12:13:12 -0500

    [Fix] condition

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -20,16 +20,28 @@ def likelihood(x, n, P):
         1D numpy.ndarray containing the likelihood of obtaining the data, x
         and n, for each probability in P, respectively.
     """
-    if type(n) is not int or n < 1:
-        raise TypeError("n must be a positive integer")
-    if type(x) is not int or x < 0:
-        raise TypeError("x must be an integer that is greater than or equal"
-                        "to 0")
-    if x > n:
+    # if type(n) is not int or n < 1:
+    #     raise TypeError("n must be a positive integer")
+    # if type(x) is not int or x < 0:
+    #     raise TypeError(
+    #         "x must be an integer that is greater than or equal to 0")
+    # if x > n:
+    #     raise ValueError("x cannot be greater than n")
+    # if type(P) is not np.ndarray or len(P.shape) != 1:
+    #     raise TypeError("P must be a 1D numpy.ndarray")
+    # if not np.all((P >= 0) & (P <= 1)):
+    #     raise ValueError("All values in P must be in the range [0, 1]")
+
+    if not isinstance(n, (int, float)) or (n <= 0):
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, (int, float)) or (x < 0):
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+    if (x > n):
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not np.all((P >= 0) & (P <= 1)):
+    if np.any(P > 1) or np.any(P < 0):
         raise ValueError("All values in P must be in the range [0, 1]")
 
     combination = np.math.factorial(n) / (np.math.factorial(n - x) *
