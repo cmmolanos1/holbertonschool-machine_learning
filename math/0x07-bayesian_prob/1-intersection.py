7f685d46de353052ae7782791a3fc62403cd0038
Carlos <926@holbertonschool.com>
2020-07-31 12:21:45 -0500

    [Fix] condition

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -21,16 +21,16 @@ def intersection(x, n, P, Pr):
         1D numpy.ndarray containing the intersection of obtaining x and n with
         each probability in P, respectively.
     """
-    if type(n) is not int or n < 1:
-        raise TypeError("n must be a positive integer")
-    if type(x) is not int or x <= 0:
-        raise TypeError("x must be an integer that is greater than or equal"
-                        "to 0")
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or (x < 0):
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
