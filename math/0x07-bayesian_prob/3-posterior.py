f51c976e6b673e2da42c6477d24e1843a143a5fe
Carlos <926@holbertonschool.com>
2020-07-31 12:36:45 -0500

    [Fix] condition

--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -20,22 +20,23 @@ def posterior(x, n, P, Pr):
     Returns:
         The marginal probability of obtaining x and n.
     """
-    if type(n) is not int or n < 1:
-        raise TypeError("n must be a positive integer")
-    if type(x) is not int or x <= 0:
-        raise TypeError("x must be an integer that is greater than or equal"
-                        "to 0")
+    if not isinstance(n, int) or (n <= 0):
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or (x < 0):
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if not np.all((P >= 0) & (P <= 1)):
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P")
+    if np.any((P > 1) | (P < 0)):
         raise ValueError("All values in P must be in the range [0, 1]")
-    if not np.all((Pr >= 0) & (Pr <= 1)):
-        raise ValueError("All values in P must be in the range [0, 1]")
-    if not np.isclose(Pr.sum(), 1):
+    if np.any((Pr > 1) | (Pr < 0)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not (np.isclose(np.sum(Pr), 1)):
         raise ValueError("Pr must sum to 1")
 
     combination = np.math.factorial(n) / (np.math.factorial(n - x) *
