72136ed8775ace40b7a3bb6ed031f024617de3c3
Carlos <926@holbertonschool.com>
2020-08-04 11:07:12 -0500

    [Fix] when kmax is none.

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -34,8 +34,8 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         return None, None
     if kmax is not None and (type(kmax) is not int or kmax < 1):
         return None, None
-    # if kmin >= kmax:
-    #   return None, None
+    if kmax is not None and kmin >= kmax:
+      return None, None
     if type(iterations) is not int or iterations < 1:
         return None, None
 
