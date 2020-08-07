ee4eb362a285fe8c670d9f5d1f3709c2674252b7
Carlos <926@holbertonschool.com>
2020-08-07 12:58:07 -0500

    [Upload] task 6

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -5,7 +5,16 @@ import numpy as np
 
 
 def maximization(X, g):
-    """calculates the maximization step in the EM algorithm for a GMM
+    """calculates the maximization step in the EM algorithm for a GMM:
+
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    g is a numpy.ndarray of shape (k, n) containing the posterior probabilities for each data point in each cluster
+    You may use at most 1 loop
+    Returns: pi, m, S, or None, None, None on failure
+        pi is a numpy.ndarray of shape (k,) containing the updated priors for each cluster
+        m is a numpy.ndarray of shape (k, d) containing the updated centroid means for each cluster
+        S is a numpy.ndarray of shape (k, d, d) containing the updated covariance matrices for each cluster
+
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
