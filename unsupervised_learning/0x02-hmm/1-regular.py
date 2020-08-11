d9bd46664a4d87461d2850c45f9d7587e8d65174
Carlos <926@holbertonschool.com>
2020-08-11 09:32:13 -0500

    [Upload] task 1

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Markov chain
+"""
+
+import numpy as np
+
+
+def regular(P):
+    """ determines the probability of a markov chain being in a particular
+        state after a specified number of iterations.
+
+    Args:
+        P (np-ndarray): of shape (n, n) representing the transition matrix.
+                        - P[i, j] is the probability of transitioning from
+                          state i to state j.
+                        - n is the number of states in the markov chain.
+    Returns:
+         a numpy.ndarray of shape (1, n) containing the steady state
+         probabilities, or None on failure.
+    """
+    if type(P) is not np.ndarray or len(P.shape) != 2 or \
+            P.shape[0] != P.shape[1]:
+        return None
+
+    if np.any(P <= 0):
+        return None
+    else:
+        A = P.T - np.eye(P.shape[0])
+        A[-1] = np.ones((P.shape[0]))
+        v = np.zeros(P.shape[0])
+        v[-1] = 1
+        s = np.linalg.solve(A, v)
+
+    return s
