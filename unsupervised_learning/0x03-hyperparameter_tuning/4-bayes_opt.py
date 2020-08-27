fceed5f986573c0935740c3d4869ebfab1d8fc1d
Carlos <926@holbertonschool.com>
2020-08-27 18:58:12 -0500

    [Upload] task 4

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 """
-Bayesian
-Based from: http://krasserm.github.io/2018/03/21/bayesian-optimization/
+Bayesian optimization
 """
 
 import numpy as np
 from scipy.stats import norm
-
 GP = __import__('2-gp').GaussianProcess
 
 
-class BayesianOptimization():
+class BayesianOptimization:
     """
-    Bayesian
+    Bayesian optimization
     """
-
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
                  xsi=0.01, minimize=True):
         """ Class constructor
-
         Args:
             f: the black-box function  to be optimized.
             X_init (np.ndarray): shape (t, 1) representing the inputs already
@@ -47,37 +43,30 @@ class BayesianOptimization():
 
     def acquisition(self):
         """calculates the next best sample location.
-
         Returns:
             X_next, EI
-
             - X_next is a numpy.ndarray of shape (1,) representing the next
               best sample point.
             - EI is a numpy.ndarray of shape (ac_samples,) containing the
             expected improvement of each potential sample.
         """
-        mu_sample, sigma_sample = self.gp.predict(self.X_s)
+        mu, sigma = self.gp.predict(self.X_s)
 
         if self.minimize is True:
-            Y_sample_opt = np.min(self.gp.Y)
-            imp = Y_sample_opt - mu_sample - self.xsi
+            Y_sample = np.min(self.gp.Y)
+            imp = Y_sample - mu - self.xsi
         else:
-            Y_sample_opt = np.max(self.gp.Y)
-            imp = mu_sample - Y_sample_opt - self.xsi
+            Y_sample = np.max(self.gp.Y)
+            imp = mu - Y_sample - self.xsi
 
-        # with np.errstate(divide='ignore'):
-        #     Z = imp / sigma_sample
-        #     ei = imp * norm.cdf(Z) + sigma_sample * norm.pdf(Z)
-        #     ei[sigma_sample == 0.0] = 0.0
-
-        Z = np.zeros(sigma_sample.shape[0])
-        for i in range(sigma_sample.shape[0]):
-            if sigma_sample[i] > 0:
-                Z[i] = imp[i] / sigma_sample[i]
+        Z = np.zeros(sigma.shape[0])
+        for i in range(sigma.shape[0]):
+            if sigma[i] > 0:
+                Z[i] = imp[i] / sigma[i]
             else:
                 Z[i] = 0
-            ei = imp * norm.cdf(Z) + sigma_sample * norm.pdf(Z)
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
 
         X_next = self.X_s[np.argmax(ei)]
 
-        return (X_next, ei)
+        return X_next, ei
