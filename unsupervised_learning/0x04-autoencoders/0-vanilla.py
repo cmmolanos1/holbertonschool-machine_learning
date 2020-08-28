bcce557c64bb70ad53a4e1cbf79dea350a11aec2
Carlos <926@holbertonschool.com>
2020-08-28 12:46:07 -0500

    [Upload] task 0

---
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Vanilla Encoder
+"""
+
+import tensorflow.keras as K
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates an autoencoder.
+
+    Args:
+        input_dims (int): the dimensions of the model input.
+        hidden_layers (list): the number of nodes for each hidden layer in the
+                              encoder, respectively the hidden layers should
+                              be reversed for the decoder.
+        latent_dims (int): the dimensions of the latent space representation.
+
+    Returns:
+        encoder, decoder, auto
+            - encoder is the encoder model
+            - decoder is the decoder model
+            - auto is the full autoencoder model
+    """
+    input_img = K.Input(shape=(input_dims,))
+    for i, layer in enumerate(hidden_layers):
+        if i == 0:
+            encoded = K.layers.Dense(layer, activation='relu')(input_img)
+        else:
+            encoded = K.layers.Dense(layer, activation='relu')(encoded)
+
+    botneckle = K.layers.Dense(latent_dims, activation='relu')(encoded)
+    encoder = K.models.Model(input_img, botneckle)
+
+    input_botneckle = K.Input(shape=(latent_dims,))
+    for i in range(len(hidden_layers) - 1, -1, -1):
+        if i == len(hidden_layers) - 1:
+            decoded = K.layers.Dense(
+                hidden_layers[i], activation='relu')(input_botneckle)
+        else:
+            decoded = K.layers.Dense(
+                hidden_layers[i], activation='relu')(decoded)
+    decoded = K.layers.Dense(input_dims, activation='sigmoid')(decoded)
+    decoder = K.models.Model(input_botneckle, decoded)
+
+    input_autoencoder = K.Input(shape=(input_dims,))
+    encoder_outs = encoder(input_autoencoder)
+    decoder_outs = decoder(encoder_outs)
+    autoencoder = K.models.Model(inputs=input_autoencoder,
+                                 outputs=decoder_outs)
+
+    autoencoder.compile(optimizer='Adam', loss='binary_crossentropy')
+
+    return encoder, decoder, autoencoder
