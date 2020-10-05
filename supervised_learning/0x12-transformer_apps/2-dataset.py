049c63bbc993c46d5efc1aa026cb3e162d852c6c
Carlos <926@holbertonschool.com>
2020-10-05 16:15:42 -0500

    [Upload] task2

--- supervised_learning/0x12-transformer_apps/2-dataset.py
+++ supervised_learning/0x12-transformer_apps/2-dataset.py
@@ -24,6 +24,9 @@ class Dataset:
         self.tokenizer_pt, self.tokenizer_en = \
             self.tokenize_dataset(self.data_train)
 
+        self.data_train = self.data_train.map(self.tf_encode)
+        self.data_valid = self.data_valid.map(self.tf_encode)
+
     def tokenize_dataset(self, data):
         """Creates sub-word tokenizers for our dataset.
 
