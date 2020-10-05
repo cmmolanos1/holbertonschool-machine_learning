8d63b2b5625040c63be030eeff3b5478ff93a7b1
Carlos <926@holbertonschool.com>
2020-10-05 11:07:17 -0500

    [Upload] task0

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -16,12 +16,11 @@ class Dataset:
         """
         Class constructor
         """
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split=['train'],
-                                    as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split=['validation'],
-                                    as_supervised=True)
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True,
+                                       as_supervised=True)
+        self.data_train, self.data_valid = examples['train'], \
+            examples['validation']
 
         self.tokenizer_pt, self.tokenizer_en = \
             self.tokenize_dataset(self.data_train)
