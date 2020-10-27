70ee6560008afa2c7102aa13c3a5dbfd11e9f533
Carlos <926@holbertonschool.com>
2020-10-27 09:18:54 -0500

    [Upload] task 2

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Retrieving user location
+"""
+import requests
+import sys
+import time
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+
+        print(r.json()['location'])
+
+    elif r.status_code == 404:
+        print("Not found")
+
+    elif r.status_code == 403:
+        limit_time = r.headers['X-Ratelimit-Reset']
+        now = time.time()
+        remaining = int((limit_time - now) / 60)
+        print("Reset in {} min".format(remaining))
