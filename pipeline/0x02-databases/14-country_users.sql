1b9dbe1c93e0fd5393388853898750e7a65a1227
Carlos <926@holbertonschool.com>
2020-11-02 11:47:43 -0500

    [Uploas]: tasks 14

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,9 @@
+-- CREATES A TABLE users
+CREATE TABLE IF NOT EXISTS users
+(
+    id      INT          NOT NULL AUTO_INCREMENT,
+    email   VARCHAR(255) NOT NULL UNIQUE,
+    name    VARCHAR(255),
+    country ENUM('US', 'CO', 'TN'),
+    PRIMARY KEY (id)
+);
