987c7c02d33f6f732c730a7b3b9a46eee9038121
Carlos <926@holbertonschool.com>
2020-11-02 12:55:17 -0500

    [Uploas]: tasks 16

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,3 +1,4 @@
+--  lists all bands with Glam Rock as their main style, ranked by their longevity
 SELECT band_name, IF(split IS NULL, YEAR(CURDATE()), split) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE concat('%', 'Glam Rock', '%')
