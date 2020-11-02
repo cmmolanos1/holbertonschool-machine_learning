cbf828e6893e70ee990c24f003cacc40c76c44f3
Carlos <926@holbertonschool.com>
2020-11-02 10:20:04 -0500

    [Fix]: uppercased keywords.

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- NUMBER OF SHOWS BY GENRE
-SELECT tv_genres.name as genre, COUNT(*) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
          JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
