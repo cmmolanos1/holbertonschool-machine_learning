a74f58f65ec981d5f92e802758701ea717c98f09
Carlos <926@holbertonschool.com>
2020-11-02 18:47:41 -0500

    [Upload]: tasks 19

---
+++ pipeline/0x02-databases/19-bonus.sql
@@ -0,0 +1,17 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
+delimiter //
+CREATE PROCEDURE AddBonus(IN user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+    SET @count = (SELECT COUNT(*) FROM projects WHERE projects.name LIKE project_name);
+    IF @count = 0 THEN
+        INSERT INTO projects
+        VALUES (project_name);
+    END IF;
+
+    -- SET @project_id = (SELECT id FROM projects WHERE name = project_name);
+
+    INSERT INTO corrections
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END
+//
+delimiter ;
