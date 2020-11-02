1d9000f15d62a6f6a6ac4ba705fc352c6dff5563
Carlos <926@holbertonschool.com>
2020-11-02 12:12:24 -0500

    [Uploas]: tasks 15

--- pipeline/0x02-databases/15-fans.sql
+++ pipeline/0x02-databases/15-fans.sql
@@ -1,5 +1,5 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, SUM(fans) as fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY fans DESC;
+ORDER BY nb_fans DESC;
