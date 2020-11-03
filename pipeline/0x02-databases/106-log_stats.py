5f3a9b128048e05449b8265466d1861ca20eb689
Carlos <926@holbertonschool.com>
2020-11-03 16:18:54 -0500

    [Upload]: tasks 41

---
+++ pipeline/0x02-databases/106-log_stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+nginx logs
+"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    # cursor = collection.find({})
+    # print(cursor[0])
+
+    number_logs = collection.count_documents({})
+    print("{} logs".format(number_logs))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        query = {"method": method}
+        count_methods = collection.count_documents(query)
+        print("\tmethod {}: {}".format(method, count_methods))
+
+    query2 = {"method": "GET", "path": "/status"}
+    count2 = collection.count_documents(query2)
+    print("{} status check".format(count2))
+
+    print("IPs:")
+
+    pipeline = [
+        {"$sortByCount": '$ip'},
+        {"$sort": {"ip": -1}}
+    ]
+    ips = collection.aggregate(pipeline)
+
+    num_elements = 0
+    for ip in ips:
+        if num_elements == 10:
+            break
+
+        print("\t{}: {}".format(ip['_id'], ip['count']))
+        num_elements += 1
